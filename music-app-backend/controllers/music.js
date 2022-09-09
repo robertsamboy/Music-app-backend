a0d4e826dad196b8db99d641afb5042c78e6efa0
robertsamboy <106219442+robertsamboy@users.noreply.github.com>
2022-09-09 09:19:28 -0400

    added some routes...

--- music-app-backend/controllers/music.js
+++ music-app-backend/controllers/music.js
@@ -4,6 +4,27 @@ const Musics = require('../models/music');
 
 
 
+//SOON TO BE INDEX ROUTE
+router.get('/', (req, res)=>{
+    Todos.find({}, (err, foundSong)=>{
+        res.json(foundSong);
+    });
+});
+
+//DELETE ROUTE
+router.delete('/:id', (req, res)=>{
+    Musics.findByIdAndRemove(req.params.id, (err, deletedSong)=>{
+        res.json(deletedSong);
+    });
+});
+
+
+//UPDATE ROUTE
+router.put('/:id', (req, res)=>{
+    Musics.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedSong)=>{
+        res.json(updatedSong);
+    });
+});
 
 
 
