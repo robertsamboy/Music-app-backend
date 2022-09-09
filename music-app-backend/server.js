44a20f10133d6ca95fc4354eaa0b91ac789cef92
robertsamboy <106219442+robertsamboy@users.noreply.github.com>
2022-09-09 08:30:51 -0400

    Update server.js

--- music-app-backend/server.js
+++ music-app-backend/server.js
@@ -1,2 +1,23 @@
+// Dependencies
 const express = require('express')
-const mongoose = 
+const mongoose = require('mongoose')
+
+// Environment Variables (getting ready for Heroku)
+const app = express();
+const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/merncrud'
+const PORT = process.env.PORT || 3001
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log('Let\'s get things done on port', PORT)
+})
